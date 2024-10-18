7636b6f2d04d73984587820e66a1eb2c1b59fefa
hanheum <cooper13210@gmail.com>
2024-10-18 15:24:25 +0900

    8차 커밋 (10.18) 백엔드 주소 CORS 설정(찐수정)

--- src/main/java/com/example/demo/config/CorsMvcConfig.java
+++ src/main/java/com/example/demo/config/CorsMvcConfig.java
@@ -12,7 +12,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://fitpin-web-back.kro.kr")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
