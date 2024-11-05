87fd34c93bcff64cc902acce564fcdb0f1b7a59f
Lseoksee <da864268@naver.com>
2024-11-05 22:45:08 +0900

    웹 소켓 추가

--- src/main/java/com/example/demo/websocket/WebScoketController.java
+++ src/main/java/com/example/demo/websocket/WebScoketController.java
@@ -14,14 +14,16 @@ public class WebScoketController {
 
 	private final SimpMessagingTemplate messagingTemplate;
 
-	// 클라이언트 메시지가 전송되는 곳 
+	// 클라이언트 메시지가 전송되는 곳
 	// 이때 경로는 setApplicationDestinationPrefixes+설정값 즉 /recv/
 	@MessageMapping("/")
-	public void clinetResponse(RecvDTO message) throws Exception {
-		log.info(message.getToken());
+	private void clinetResponse(RecvDTO message) throws Exception {
+		log.info("클라이언트 접속: " + message.getToken());
 	}
 
-	/** 웹소켓에 데이터를 전송합니다
+	/**
+	 * 웹소켓에 데이터를 전송합니다
+	 * 
 	 * @param sendData 전송할 데이터
 	 */
 	public void sendClient(ActionDTO sendData) {
