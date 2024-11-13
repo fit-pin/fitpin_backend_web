fa97ddc7133d886bc6cbe51a19867268656cc0c0
Lseoksee <da864268@naver.com>
2024-11-13 22:38:22 +0900

    /logout 경로도 추가

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -28,104 +28,120 @@ import java.util.Collections;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
-    private final AuthenticationConfiguration authenticationConfiguration;
+        // AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+        private final AuthenticationConfiguration authenticationConfiguration;
 
-    private final JWTUtil jwtUtil;
+        private final JWTUtil jwtUtil;
 
-    private final RefreshRepository refreshRepository;
+        private final RefreshRepository refreshRepository;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil, RefreshRepository refreshRepository) {
+        public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil,
+                        RefreshRepository refreshRepository) {
 
-        this.authenticationConfiguration = authenticationConfiguration;
-        this.jwtUtil = jwtUtil;
-        this.refreshRepository = refreshRepository;
-    }
+                this.authenticationConfiguration = authenticationConfiguration;
+                this.jwtUtil = jwtUtil;
+                this.refreshRepository = refreshRepository;
+        }
 
-    //AuthenticationManager Bean 등록
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        // AuthenticationManager Bean 등록
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
 
-        return configuration.getAuthenticationManager();
-    }
+                return configuration.getAuthenticationManager();
+        }
 
-    //암호화를 위해 사용
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        // 암호화를 위해 사용
+        @Bean
+        public BCryptPasswordEncoder bCryptPasswordEncoder() {
 
-        return new BCryptPasswordEncoder();
-    }
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        //CORS 설정
-        http
-                .cors((cors) -> cors
-                        .configurationSource(new CorsConfigurationSource() {
+                // CORS 설정
+                http
+                                .cors((cors) -> cors
+                                                .configurationSource(new CorsConfigurationSource() {
 
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                                        @Override
+                                                        public CorsConfiguration getCorsConfiguration(
+                                                                        HttpServletRequest request) {
 
-                        CorsConfiguration configuration = new CorsConfiguration();
+                                                                CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://fitpin-web-back.kro.kr"
-                                                                    ,"https://fit-pin.github.io", "http://localhost", "http://korseok.kro.kr"));
-                        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
-                        configuration.setMaxAge(3600L);
+                                                                configuration.setAllowedOrigins(Arrays.asList(
+                                                                                "http://localhost:3000",
+                                                                                "http://fitpin-web-back.kro.kr",
+                                                                                "https://fit-pin.github.io",
+                                                                                "http://localhost",
+                                                                                "http://korseok.kro.kr"));
+                                                                configuration.setAllowedMethods(Arrays.asList("GET",
+                                                                                "POST", "PUT", "DELETE", "OPTIONS"));
+                                                                configuration.setAllowedHeaders(
+                                                                                Collections.singletonList("*"));
+                                                                configuration.setAllowCredentials(true);
+                                                                configuration.setExposedHeaders(Arrays.asList(
+                                                                                "Authorization", "Content-Type"));
+                                                                configuration.setMaxAge(3600L);
 
-                        return configuration;
-                    }
-                }));
+                                                                return configuration;
+                                                        }
+                                                }));
 
-        //csrf disable
-        http
-                .csrf((auth) -> auth.disable());
+                // csrf disable
+                http
+                                .csrf((auth) -> auth.disable());
 
-        //From 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
+                // From 로그인 방식 disable
+                http
+                                .formLogin((auth) -> auth.disable());
 
-        //http basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
+                // http basic 인증 방식 disable
+                http
+                                .httpBasic((auth) -> auth.disable());
 
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join","/check-username","/users/**").permitAll()
-                        .requestMatchers("/token","/inquiry/**","list","/inquiryImg/**", "/getauction/**").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/reissue").permitAll()
-                        // 웹 소켓 안되는게 이거 문제였누...
-                        .requestMatchers("/sock").permitAll()
-                        .requestMatchers("/action").permitAll()
-                        .requestMatchers("/recv").permitAll()
-                        .requestMatchers("/weborder").permitAll()
-                        .requestMatchers("/auction_listener/**").permitAll()
-                        .anyRequest().authenticated());
+                // 경로별 인가 작업
+                http
+                                .authorizeHttpRequests((auth) -> auth
+                                                .requestMatchers("/login", "/", "/join", "/check-username", "/users/**" , "/logout")
+                                                .permitAll()
+                                                .requestMatchers("/token", "/inquiry/**", "list", "/inquiryImg/**",
+                                                                "/getauction/**")
+                                                .permitAll()
+                                                .requestMatchers("/admin").hasRole("ADMIN")
+                                                .requestMatchers("/reissue").permitAll()
+                                                // 웹 소켓 안되는게 이거 문제였누...
+                                                .requestMatchers("/sock").permitAll()
+                                                .requestMatchers("/action").permitAll()
+                                                .requestMatchers("/recv").permitAll()
+                                                .requestMatchers("/weborder").permitAll()
+                                                .requestMatchers("/auction_listener/**").permitAll()
+                                                .anyRequest().authenticated());
 
-        //JWTFilter 등록
-        http
-                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+                // JWTFilter 등록
+                http
+                                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
-        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
-        //AuthenticationManager()와 JWTUtil 인수 전달
-        http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
+                // 필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에
+                // authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+                // AuthenticationManager()와 JWTUtil 인수 전달
+                http
+                                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),
+                                                jwtUtil, refreshRepository),
+                                                UsernamePasswordAuthenticationFilter.class);
 
-        //LogoutFilter 등록
-        http
-                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
+                // LogoutFilter 등록
+                http
+                                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository),
+                                                LogoutFilter.class);
 
-        //세션 설정
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                // 세션 설정
+                http
+                                .sessionManagement((session) -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        return http.build();
-    }
+                return http.build();
+        }
 }
